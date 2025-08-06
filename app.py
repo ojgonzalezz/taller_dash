41d38d1c4f6da4e4d6b45c5db4198d5630960ae6
ojgonzalezz <ojgonzalezz27@gmail.com>
2025-08-06 12:56:11 -0300

    nueva funcion para cargar datos

--- app.py
+++ app.py
@@ -22,7 +22,12 @@ app.config.suppress_callback_exceptions = True
 # Load data from csv
 def load_data():
     # To do: Completar la función 
-    
+    df = pd.read_csv('datos_energia.csv')
+    df['fecha'] = pd.to_datetime(df['time'])
+
+    df.set_index('fecha', inplace=True)
+
+    return df
 
 # Cargar datos
 data = load_data()
@@ -240,4 +245,5 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    #app.run_server(debug=True)
+    app.run(debug=True)
